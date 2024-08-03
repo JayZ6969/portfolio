547f055eecad99513ff7259f20fb3cf4a98c1710
Jayasheel Vinay <jayasheelvinayj@gmail.com>
2024-08-03 18:42:24 +0530

    modify: CopyrightFooter

--- src/components/CopyrightFooter/CopyrightFooter.js
+++ src/components/CopyrightFooter/CopyrightFooter.js
@@ -1,12 +1,20 @@
-import React from 'react'
-import './CopyrightFooter.css'
+import React from "react";
+import "./CopyrightFooter.css";
 
 const CopyrightFooter = (props) => {
   return (
     <div className={props.ColorBG}>
-        <p>© 2024 Jayasheel Vinay J. All rights reserved.</p>
+      <p>Coded with ❣️</p>
+      <p> © 2024 Jayasheel Vinay J. All rights reserved.</p>
+      <a
+        href="https://github.com/JayZ6969/portfolio"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Source Code 🔧
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default CopyrightFooter
+export default CopyrightFooter;
