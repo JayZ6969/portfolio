f7c08b5860fcb176878b8239ee7a99ce36fc5cec
Jayasheel Vinay <jayasheelvinayj@gmail.com>
2024-08-03 20:26:41 +0530

    modify: contact section

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -23,9 +23,7 @@ const Contact = () => {
         </div>
         <div className="contact-me">
           <h1>
-            Let's
-            <br />
-            Connect
+            Let's <br className="hide"/>Connect
           </h1>
           <div className="contact-info">
             <div className="sub-contact-info">
