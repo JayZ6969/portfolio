f61dba7fc3c302f3a8f7651833e5138b5aaa6377
Jayasheel Vinay <jayasheelvinayj@gmail.com>
2025-01-12 19:43:32 +0530

    add: animation, gsap

--- components/Featured.tsx
+++ components/Featured.tsx
@@ -1,5 +1,7 @@
-import { FC } from 'react';
+import { FC, useState, useEffect } from 'react';
 import Image from 'next/image';
+import { Power4 } from 'gsap';
+import { motion } from 'framer-motion';
 import IEEE from '../public/assets/images/IEEE.png';
 import FORCE from '../public/assets/images/FORCE.png';
 
@@ -12,13 +14,72 @@ const classes = {
   dot: 'xl:w-4 xl:h-4 lg:h-3 lg:w-3 h-2 w-2 bg-black rounded-full',
   CardHeader: 'font-inter font-semibold text-black min-[1921px]:text-[1.249vw] min-[1440px]:text-[1.5rem] xl:text-[1.25rem] text-[1rem] leading-none',
   Card: 'flex flex-col gap-6 md:w-1/2 w-full',
-  ImageContainer: 'relative w-full xl:h-[60vh] lg:h-[50vh] sm:h-[40vh] h-[30vh] max-[1440px]:max-h-[600px] rounded-xl',
+  ImageContainer: 'relative w-full cursor-pointer xl:h-[60vh] lg:h-[50vh] sm:h-[40vh] h-[30vh] max-[1440px]:max-h-[600px] rounded-xl',
   CardImage: 'flex items-center justify-center w-full h-full rounded-xl overflow-hidden shadow-lg',
   Image: 'w-[75%] rounded-lg h-auto',
-  CardImageHeader: 'absolute md:block hidden w-screen text-[#DFFF00] font-inter text-center font-bold leading-none tracking-tight min-[1921px]:text-[4.997vw] xl:text-8xl md:text-6xl sm:text-4xl text-3xl z-20 top-1/2 -translate-y-1/2 rounded-md',
+  CardImageHeader: 'absolute flex overflow-hidden w-full min-[1921px]:h-[4.997vw] xl:h-[6rem] md:h-[3.75rem] sm:h-[2.25rem] h-[1.875rem] text-[#cee968] font-inter text-center font-bold leading-none tracking-tight min-[1921px]:text-[4.997vw] xl:text-8xl md:text-6xl sm:text-4xl text-3xl z-20 top-1/2 -translate-y-1/2 rounded-md',
+  CardImageHeaderTagline: 'font-inter flex items-center justify-center h-full',
 }
 
 const Featured: FC = () => {
+
+  motion
+
+  const [isHovering1, setHovering1] = useState(false);
+  const [isHovering2, setHovering2] = useState(false);
+  const [isHovering3, setHovering3] = useState(false);
+  const [isHovering4, setHovering4] = useState(false);
+
+  const useMediaQuery = (query: string) => {
+    const [matches, setMatches] = useState(false);
+  
+    useEffect(() => {
+      const media = window.matchMedia(query);
+      if (media.matches !== matches) {
+        setMatches(media.matches);
+      }
+      const listener = () => setMatches(media.matches);
+      media.addEventListener('change', listener);
+      return () => media.removeEventListener('change', listener);
+    }, [matches, query]);
+  
+    return matches;
+  };
+  
+  const isMdUp = useMediaQuery('(min-width: 768px)');
+
+  const handleMouseEnter1 = () => {
+    if (isMdUp) setHovering1(true);
+  };
+
+  const handleMouseLeave1 = () => {
+    if (isMdUp) setHovering1(false);
+  };
+
+  const handleMouseEnter2 = () => {
+    if (isMdUp) setHovering2(true);
+  };
+
+  const handleMouseLeave2 = () => {
+    if (isMdUp) setHovering2(false);
+  };
+
+  const handleMouseEnter3 = () => {
+    if (isMdUp) setHovering3(true);
+  };
+
+  const handleMouseLeave3 = () => {
+    if (isMdUp) setHovering3(false);
+  };
+
+  const handleMouseEnter4 = () => {
+    if (isMdUp) setHovering4(true);
+  };
+
+  const handleMouseLeave4 = () => {
+    if (isMdUp) setHovering4(false);
+  };
+
   return (
     <div className={classes.Container}>
       <div className={classes.FeaturedContainer}>
@@ -29,9 +90,9 @@ const Featured: FC = () => {
               <div className={classes.dot}></div>
               <h1 className={classes.CardHeader}>IEEE - ICAECC Website</h1>
             </div>
-            <div className={classes.ImageContainer}>
+            <div onMouseEnter={handleMouseEnter1} onMouseLeave={handleMouseLeave1} className={classes.ImageContainer}>
               <h1 className={` ${classes.CardImageHeader} -translate-x-1/2 md:left-full left-1/2`}>
-                {"FULL-STACK".split('').map((letter, index) => <span className='font-inter' key={index}>{letter}</span>)}
+                {"FULL-STACK".split('').map((letter, index) => <motion.span key={index} initial={{y: "100%"}} animate={isHovering1 ? ({y: "0"}) : ({y: "100%"})} transition={{ease: Power4.easeInOut, delay: index * 0.025}} className={classes.CardImageHeaderTagline}>{letter}</motion.span>)}
               </h1>
               <div className={`${classes.CardImage} bg-gray-400`}>
                 <Image className={classes.Image} src={IEEE} alt="IEEE - ICAECC Website" />
@@ -43,9 +104,9 @@ const Featured: FC = () => {
               <div className={classes.dot}></div>
               <h1 className={classes.CardHeader}>FORCE - Forum Website</h1>
             </div>
-            <div className={classes.ImageContainer}>
+            <div onMouseEnter={handleMouseEnter2} onMouseLeave={handleMouseLeave2} className={classes.ImageContainer}>
               <h1 className={` ${classes.CardImageHeader} translate-x-1/2 md:right-full right-1/2`}>
-                {"".split('').map((letter, index) => <span className='font-inter' key={index}>{letter}</span>)}
+                {"FRONT-END".split('').map((letter, index) => <motion.span key={index} initial={{y: "100%"}} animate={isHovering2 ? ({y: "0"}) : ({y: "100%"})} transition={{ease: Power4.easeInOut, delay: index * 0.025}} className={classes.CardImageHeaderTagline}>{letter}</motion.span>)}
               </h1>
               <div className={`${classes.CardImage} bg-gray-400`}>
                 <Image className={classes.Image} src={FORCE} alt="FORCE - Forum Website" />
@@ -59,9 +120,9 @@ const Featured: FC = () => {
               <div className={classes.dot}></div>
               <h1 className={classes.CardHeader}>FORCE - Forum Website</h1>
             </div>
-            <div className={classes.ImageContainer}>
+            <div onMouseEnter={handleMouseEnter3} onMouseLeave={handleMouseLeave3} className={classes.ImageContainer}>
               <h1 className={` ${classes.CardImageHeader} -translate-x-1/2 md:left-full left-1/2`}>
-                {"FRONT-END".split('').map((letter, index) => <span className='font-inter' key={index}>{letter}</span>)}
+                {"FRONT-END".split('').map((letter, index) => <motion.span key={index} initial={{y: "100%"}} animate={isHovering3 ? ({y: "0"}) : ({y: "100%"})} transition={{ease: Power4.easeInOut, delay: index * 0.025}} className={classes.CardImageHeaderTagline}>{letter}</motion.span>)}
               </h1>
               <div className={`${classes.CardImage} bg-gray-400`}>
                 <Image className={classes.Image} src={FORCE} alt="FORCE - Forum Website" />
@@ -73,9 +134,9 @@ const Featured: FC = () => {
               <div className={classes.dot}></div>
               <h1 className={classes.CardHeader}>IEEE - ICAECC Website</h1>
             </div>
-            <div className={classes.ImageContainer}>
+            <div onMouseEnter={handleMouseEnter4} onMouseLeave={handleMouseLeave4} className={classes.ImageContainer}>  
               <h1 className={` ${classes.CardImageHeader} translate-x-1/2 md:right-full right-1/2`}>
-                {"".split('').map((letter, index) => <span className='font-inter' key={index}>{letter}</span>)}
+                {"FULL-STACK".split('').map((letter, index) => <motion.span key={index} initial={{y: "100%"}} animate={isHovering4 ? ({y: "0"}) : ({y: "100%"})} transition={{ease: Power4.easeInOut, delay: index * 0.025}} className={classes.CardImageHeaderTagline}>{letter}</motion.span>)}
               </h1>
               <div className={`${classes.CardImage} bg-gray-400`}>
                 <Image className={classes.Image} src={IEEE} alt="IEEE - ICAECC Website" />
